0a03890ef62c79c5f16c0b0c693aac96759aa305
Sebastian Asis <sasisromero10@gmail.com>
2021-01-25 20:07:25 -0500

    Navbar - Hero - Services - Membership Section was created

---
+++ app.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+const body = document.querySelector('body');
+
+//Display mobile menu
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
