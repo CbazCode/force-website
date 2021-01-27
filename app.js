ec8c96dac8f0b82328e814b25542f6fdefb7481c
Sebastian Asis <sasisromero10@gmail.com>
2021-01-27 17:50:59 -0500

    Implementig GSAP

--- app.js
+++ app.js
@@ -12,3 +12,65 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+//Animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-hero', {
+    duration: 0.3,
+    opacity:0,
+    y:-150,
+    stagger:0.3
+});
+
+gsap.from('.animate-services', {
+    scrollTrigger: '.animate-services',
+    duration: 0.5,
+    opacity:1,
+    x:-150,
+    stagger:0.12
+});
+
+gsap.from('.animate-img', {
+    scrollTrigger: '.animate-img',
+    duration: 1.2,
+    opacity:0,
+    x:-200,
+});
+
+gsap.from('.animate-membership', {
+    scrollTrigger: '.animate-membership',
+    duration: 1,
+    opacity:0,
+    y:-200,
+    stagger: 0.3,
+    delay:0.5
+});
+
+gsap.from('.animate-card', {
+    scrollTrigger: '.animate-card',
+    duration: 1,
+    opacity:0,
+    y:-150,
+    stagger: 0.1,
+    delay:0.2
+});
+
+gsap.from('.animate-team', {
+    scrollTrigger: '.animate-team',
+    duration: 1,
+    opacity:0,
+    y:-150,
+    stagger: 0.3,
+    delay:0.2
+});
+
+gsap.from('.animate-email', {
+    scrollTrigger: '.animate-email',
+    duration: 0.8,
+    opacity:0,
+    y:-150,
+    stagger: 0.25,
+    delay:0.4
+});
+
